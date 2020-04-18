7bd70846c796f5559b0c8bbf322cbe131d4d2aa7
Jake White <squirrelboy1225@gmail.com>
2020-04-18 14:49:49 -0400

    early stuff

---
+++ Assets/BattleAnimator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleAnimator : MonoBehaviour
+{
+    Vector3 originalPos;
+    bool movingToCenter = false;
+    float startedMoving;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Animator>().Play("BattleStart");
+        originalPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(movingToCenter) {
+            transform.position = Vector3.Lerp(originalPos, Vector3.zero, (Time.time - startedMoving)*1.8f);
+        }
+    }
+
+    public void MoveToCenter() {
+        startedMoving = Time.time;
+        movingToCenter = true;
+    }
+
+    public void StartBattle() {
+        InputManager.instance.Begin();
+    }
+
+    public void Kill() {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Person.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Pathfinding;
+
+public class Person : MonoBehaviour
+{
+    public float proficiency;
+    public Transform target;
+    public Animator sprite;
+    public GameObject highlight, battleanim;
+    AIPath ai;
+    bool dragging;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ai = GetComponent<AIPath>();
+        ai.destination = target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sprite.SetBool("Walking", (ai.velocity.magnitude > 0));
+        if(dragging) {
+            var mouse = Input.mousePosition;
+            mouse = Camera.main.ScreenToWorldPoint(mouse);
+            mouse.z = 0;
+            transform.position = mouse;
+        }
+    }
+
+    public float GetProficiency() {
+        return proficiency;
+    }
+
+    public void Interact() {
+        GameObject newbattleanim = Instantiate(battleanim, transform.position, Quaternion.identity);
+    }
+
+    public void OnMouseOver() {
+        highlight.SetActive(true);
+    }
+
+    public void OnMouseExit() {
+        highlight.SetActive(false);
+    }
+
+    public void OnMouseDown() {
+        ai.isStopped = true;
+        dragging = true;
+    }
+    public void OnMouseUp() {
+        dragging = false;
+        Interact();
+    }
+}
